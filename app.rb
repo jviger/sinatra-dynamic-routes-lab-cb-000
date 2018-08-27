9666aea9c17d4807b98f82dfc0e8233846d931b4
Jason Viger <jason.viger@gmail.com>
2018-08-27 11:18:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -33,6 +33,8 @@ class App < Sinatra::Base
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
     @number1 = params[:number1]
+    @number2 = params[:number2]
+    
   end
   
 end
