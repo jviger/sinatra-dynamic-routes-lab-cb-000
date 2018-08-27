68919105c6a8c94005d7c91adcdae7c03beb2757
Jason Viger <jason.viger@gmail.com>
2018-08-27 10:50:52 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
   end
   
   get "/square/:number" do
-    @square = params[:number].to_i *
+    @square = params[:number].to_i * params[:number].to_i
     "Hello #{@reversed_name}!"
   end
   
